6833708850072d724a1572010847f9cd96091782
andriyl <andrii.leliv@gmail.com>
2016-01-27 19:10:37 +0200

    Updated publications

--- server/publications.js
+++ server/publications.js
@@ -1,3 +1,15 @@
+Meteor.publish("locations", function () {
+    return Locations.find();
+});
+
+Meteor.publish(null, function() {
+    return Meteor.users.find({_id: this.userId}, {fields: {place_id: 1, location_id: 1}});
+});
+
 Meteor.publish("messages", function () {
     return Messages.find();
+});
+
+Meteor.publish("users", function () {
+    return Meteor.users.find({}, {fields: {profile: 1} });
 });
