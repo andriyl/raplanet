fa5880591fc8c73058f86e75ab2220c78a5f06a5
andriyl <andrii.leliv@gmail.com>
2016-01-27 17:00:10 +0200

    Added Google search locations for 'Edit profile' page

--- client/templates/profile/editProfile.js
+++ client/templates/profile/editProfile.js
@@ -14,6 +14,28 @@ Schema.User = new SimpleSchema({
     "emails.$.address": {
         type: String,
         regEx: SimpleSchema.RegEx.Email
+    },
+    "profile.place": {
+        type: String,
+        label: "Location",
+        autoform: {
+            type: "placecomplete",
+            placecompleteOptions: {
+                allowClear: true,
+                requestParams: function(){},
+                selectDetails: function(placeResult) {
+                    $("input[name='place_id']").val(placeResult.place_id);
+                    return null;
+                }
+            }
+        }
+    },
+    "place_id": {
+        type: String,
+        autoform: {
+            type: "hidden",
+            label: false
+        }
     }
 });
 
