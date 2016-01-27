db04e49375f777caaa750fb5f51f5a9e136a7cd5
andriyl <andrii.leliv@gmail.com>
2016-01-27 19:08:50 +0200

    Messages functionality and styling

---
+++ client/templates/messages/messages.js
@@ -0,0 +1,34 @@
+Template.messages.helpers({
+    posts: function() {
+        var
+        locationId = Locations.findOne({place_id: Meteor.user().place_id}),
+
+        posts = Messages.find({location_id: locationId}, {sort: {createdAt: -1}}).fetch();
+        posts.map(function(post){
+             post.user = Meteor.users.findOne({_id: post.user_id},{fields: {profile: 1}});
+        });
+
+        return posts;
+    },
+    place: function(){
+        return Meteor.user().profile.place;
+    }
+});
+
+Template.registerHelper('messageDate', function(date) {
+    return moment(date).fromNow();
+});
+
+Template.messages.events({
+    'submit': function(e){
+        e.preventDefault();
+
+        var postValue = e.target.post.value;
+        if(!postValue) return false;
+
+        Meteor.call('insertPost', postValue, function(error, result) {
+            if (error) return alert(error.reason);
+            e.target.post.value = '';
+        });
+    }
+});
