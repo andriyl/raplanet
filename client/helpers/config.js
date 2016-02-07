d6b9189057697de7ce8fe15b679d80ecfb7dc374
andriyl <andrii.leliv@gmail.com>
2016-02-07 14:56:54 +0200

    Added user avatar functionality

--- client/helpers/config.js
+++ client/helpers/config.js
@@ -1,3 +1,10 @@
 Template.registerHelper('errorPages', function(page) {
     return Meteor.settings.public.errorPage[page];
 });
+
+Avatar.setOptions({
+    customImageProperty: function() {
+        var user        = this;
+        user.username   = user.profile.username;
+    }
+});
